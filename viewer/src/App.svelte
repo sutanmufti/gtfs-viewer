612cf4042d12248c54348bc17cb2a9a233b8d828
sutan mufti <upsutan@gmail.com>
2026-03-11 12:44:10 +0700

    control the sidebar

--- viewer/src/App.svelte
+++ viewer/src/App.svelte
@@ -3,14 +3,20 @@
   import Map from './Components/Map.svelte'
   import RecordViewer from './Components/RecordViewer.svelte';
   import Sidebar from './Components/Sidebar.svelte'
+  import TripsInStop from './Components/TripsInStop.svelte';
   import Viewer from './Components/Viewer.svelte'
 </script>
 
 <main class="grid grid-cols-4 w-screen h-screen overflow-hidden">
+
+  {#if appstate.stopShowRoute}
+      <TripsInStop/>
+    {:else}
   {#if appstate.activeRecord}
-    <RecordViewer/>
+  <RecordViewer/>
   {:else}
   <Sidebar />
+  {/if}
   {/if}
   <div class="col-span-3 grid grid-rows-4 overflow-auto">
     <div class="row-span-3">
---
+++ viewer/src/Components/TripsInStop.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+  import { clearStopView } from "./app.svelte";
+
+</script>
+<div class="col-span-1 flex flex-col overflow-hidden border-r border-gray-200 bg-gray-50">
+    <div>
+              <button onclick={clearStopView}>
+        Clear Stop View
+      </button>
+    </div>
+</div>
